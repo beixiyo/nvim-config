ed5bbcc1c3ed201f678022f83ea8ae603f0066ae
CJL <2662442385@qq.com>
2026-02-15 23:01:06 +0800

    refolder rename

--- my-nvim/lua/plugins/init.lua
+++ my-nvim/lua/plugins/init.lua
@@ -1,19 +1,23 @@
 -- ================================
 -- 插件列表入口
 -- ================================
--- 在此返回插件 spec，或通过 { import = "plugins.xxx" } 引入子模块
+-- 按分类引入：ui / code / file / tools
 
 return {
-  { import = "plugins.theme" },
-  { import = "plugins.which-key" },
-  { import = "plugins.fzf" },
-  { import = "plugins.alpha" },
-  { import = "plugins.neo-tree" },
-  { import = "plugins.bufferline" },
-  { import = "plugins.lualine" },
-  { import = "plugins.noice" },
-  { import = "plugins.blink" },
-  { import = "plugins.render-markdown" },
-  { import = "plugins.toggleterm" },
-  { import = "plugins.treesitter" },
+  -- UI：主题、欢迎页、状态栏、buffer 标签、消息
+  { import = "plugins.ui.theme" },
+  { import = "plugins.ui.alpha" },
+  { import = "plugins.ui.bufferline" },
+  { import = "plugins.ui.lualine" },
+  { import = "plugins.ui.noice" },
+  -- 代码：语法、补全、Markdown 渲染
+  { import = "plugins.code.treesitter" },
+  { import = "plugins.code.blink" },
+  { import = "plugins.code.render-markdown" },
+  -- 文件：文件树、模糊查找
+  { import = "plugins.file.neo-tree" },
+  { import = "plugins.file.fzf" },
+  -- 工具：键位提示、终端
+  { import = "plugins.tools.which-key" },
+  { import = "plugins.tools.toggleterm" },
 }
